d5c0f412a16f537e01caefcfb8032abf3a7eb197
Leon <lac336@cornell.edu>
2023-12-26 00:32:41 -0500

    reset hamburger

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -11,20 +11,20 @@ $("#hamburger").click(function() {
   console.log("test");
 
   if ($("nav").hasClass('hidden')) {
-    $(".maincontent").delay(.0000005).animate({opacity: "0"}); 
+    $(".maincontent").delay(.005).animate({opacity: "0"});
     $(".maincontent").addClass('hidden');
     $("nav").fadeIn();
     $("nav").animate({height: "50%"});
-    $("nav").stop(true, true).delay(.005).animate({opacity: "1"});
+    $("nav").delay(.005).animate({opacity: "1"});
     $("nav").removeClass('hidden');
 
   } else {
-    $("nav").animate({height: "0px"});
+    $("nav").delay(.005).animate({height: "0px"});
     $("nav").delay(.005).animate({opacity: "0"});
     $("main").removeClass('hidden');
     $("footer").removeClass('hidden');
     $(".maincontent").removeClass('hidden');
-    $(".maincontent").delay(.00000005).animate({opacity: "1"});
+     $(".maincontent").delay(.005).animate({opacity: "0"});
     $("nav").addClass('hidden');
   }
 
