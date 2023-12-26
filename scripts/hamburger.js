799dfa27cc29cd48ee2397ff4497bcd26ce9896d
Leon <lac336@cornell.edu>
2023-12-26 00:28:28 -0500

    delay changes hamburger

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -15,11 +15,11 @@ $("#hamburger").click(function() {
     $(".maincontent").addClass('hidden');
     $("nav").fadeIn();
     $("nav").animate({height: "50%"});
-    $("nav").delay(.005).animate({opacity: "1"});
+    $("nav").stop(true, true).delay(.005).animate({opacity: "1"});
     $("nav").removeClass('hidden');
 
   } else {
-    $("nav").delay(.005).animate({height: "0px"});
+    $("nav").animate({height: "0px"});
     $("nav").delay(.005).animate({opacity: "0"});
     $("main").removeClass('hidden');
     $("footer").removeClass('hidden');
