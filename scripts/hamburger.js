8dbc80d83fc411c97599a9f2e9190d271e3035d0
Leon <lac336@cornell.edu>
2023-12-25 20:15:34 -0500

    even more hamburger changes

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,16 +1,29 @@
+// $(document).ready(function(){
+//   $("nav").delay("fast").animate({height: "0px"});
+//     $("nav").delay("fast").animate({opacity: "0"});
+//     $("main").removeClass('hidden');
+//     $("footer").removeClass('hidden');
+//     $(".maincontent").removeClass('hidden');
+//     $("nav").addClass('hidden');
+// });
+
 $("#hamburger").click(function() {
   console.log("test");
 
   if ($("nav").hasClass('hidden')) {
-    $("nav").removeClass('hidden');
+    $(".maincontent").addClass('hidden');
     $("nav").fadeIn();
-    $(".maincontent").fadeOut();
+    $("nav").animate({height: "50%"});
+    $("nav").delay(.005).animate({opacity: "1"});
+    $("nav").removeClass('hidden');
 
   } else {
-    $("nav").addClass('hidden');
+    $("nav").delay(.005).animate({height: "0px"});
+    $("nav").delay(.005).animate({opacity: "0"});
     $("main").removeClass('hidden');
     $("footer").removeClass('hidden');
-    $(".maincontent").fadeIn();
+    $(".maincontent").removeClass('hidden');
+    $("nav").addClass('hidden');
   }
 
 });
